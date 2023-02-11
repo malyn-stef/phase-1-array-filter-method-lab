fca952da0ddc71d338c8286c0eb77b0c2d2d48a8
+ <+>
2023-02-10 17:20:56 -0700

    array filter methods

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(array, str) {
+  return array.filter(function (testName) {
+    return testName.toLowerCase() == str.toLowerCase()
+
+
+
+  })
+}
+
+function fuzzyMatch(array, str) {
+  return array.filter(function (returnThing) {
+    return returnThing.startsWith(str)
+  })
+}
+
+function matchName(array, str) {
+  return array.filter(function (returnThing) {
+    return returnThing["name"] === str
+  }
+
+  )
+
+}
